11f6d7c8ea447ab66e83d75ca25bd52398e6ba81
Mankar123sayu <sayumankar77@gmail.com>
2023-03-25 17:35:04 +0530

    Addfile

---
+++ DelegateAndEvent/DelegateAndEvent/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace DelegatesAndEvents
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            DelegatesDemo delegatesDemo = new DelegatesDemo();
+             delegatesDemo.Run();
+
+            // MultiCastDelegates multiCastDelegates = new MultiCastDelegates();
+            //multiCastDelegates.Run();
+
+            // EventDemo eventDemo = new EventDemo();
+            //eventDemo.Run();
+
+           
+        }
+    }
+}
